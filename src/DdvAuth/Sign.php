482486c1ab113cac7aabff91e06ec6bbd2b61472
yue lin <yuelin@yuedeiMac.local>
2017-07-14 12:17:14 +0800

    Add createGuid

--- src/DdvAuth/Sign.php
+++ src/DdvAuth/Sign.php
@@ -66,4 +66,25 @@ class Sign
     $signHeaderKeys = array_unique($signHeaderKeys);
     return $signHeaderKeys;
   }
+  //生成guid
+  public static function createGuid( $opt = false ){
+    //  Set to true/false as your default way to do this.
+    if( function_exists('com_create_guid') ){
+      return $opt?com_create_guid() : trim( com_create_guid(), '{}' );
+    }else{
+      mt_srand( (double)microtime() * 10000 );    // optional for php 4.2.0 and up.
+      $charid = strtoupper( md5(uniqid(rand(), true)) );
+      $hyphen = chr( 45 );    // "-"
+      $left_curly = $opt ? chr(123) : "";     //  "{"
+      $right_curly = $opt ? chr(125) : "";    //  "}"
+      $uuid = $left_curly
+          . substr( $charid, 0, 8 ) . $hyphen
+          . substr( $charid, 8, 4 ) . $hyphen
+          . substr( $charid, 12, 4 ) . $hyphen
+          . substr( $charid, 16, 4 ) . $hyphen
+          . substr( $charid, 20, 12 )
+          . $right_curly;
+      return $uuid;
+    }
+  }
 }
